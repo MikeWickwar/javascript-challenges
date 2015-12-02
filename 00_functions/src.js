9cd4d075dbea31f0f36c79adaa876664f0ca7c73
Mike Wickwar <mike.wickwar@gmail.com>
2015-12-02 13:32:26 -0700

    completed functions

--- 00_functions/src.js
+++ 00_functions/src.js
@@ -10,5 +10,24 @@ module.exports = {
 
   shout: function(string) {
     return "Hello " +string.toUpperCase();
+  },
+  whisper: function(string){
+    return "Hello "+string.toLowerCase();
+  },
+  separate: function(string){
+    var split = string.split('');
+    return split;
+  },
+  reverseJoin: function(array){
+    var rJoin = array.reverse().join(',');
+    return rJoin;
+  },
+  reverseJoinCompact: function(array){
+    var rJoin = array.reverse().join('');
+    return rJoin;
+  },
+  backwardsDay: function(string){
+    return 'Hello ' +string.split('').reverse().join('');
+
   }
 }
